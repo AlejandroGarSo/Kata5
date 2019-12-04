14a04675ac7ca909b7e51b3986f5120716228446
Usuario <Usuario@10.22.148.81>
2019-12-04 14:08:55 +0000

    Cleaning 1

--- src/kata5/TableInserter.java
+++ src/kata5/TableInserter.java
@@ -2,9 +2,7 @@ package kata5;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TableInserter {
     
